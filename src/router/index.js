ba5d1fa8cd2ddd4eeb1dbfad4aa1e6151e865e35
wwfendurer <425845682@qq.com>
2020-04-30 10:35:47 +0800

    day 004

--- src/router/index.js
+++ src/router/index.js
@@ -7,8 +7,8 @@ import Welcome from '../components/Welcome.vue'
 import Users from '../components/user/Users.vue'
 import Rights from '../components/power/Rights.vue'
 import Roles from '../components/power/Roles.vue'
-import Cate from './components/goods/Cate.vue'
-import Params from './components/goods/Params.vue'
+import Cate from '../components/goods/Cate.vue'
+import Params from '../components/goods/Params.vue'
 
 Vue.use(VueRouter)
 
