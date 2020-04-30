fd43964bc482674a2ec8a1c9e2bf3fc75164ff99
wwfendurer <425845682@qq.com>
2020-04-30 11:03:50 +0800

    day 005

--- src/main.js
+++ src/main.js
@@ -30,6 +30,22 @@ Vue.component('tree-table', TreeTable)
 
 Vue.config.productionTip = false
 
+// Vue.js 允许你自定义过滤器，可被用于一些常见的文本格式化。
+// 自定义一个时间过滤器
+Vue.filter('dateFormat', function(originVal) {
+  const dt = new Date(originVal)
+
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
+
 new Vue({
   router,
   store,
